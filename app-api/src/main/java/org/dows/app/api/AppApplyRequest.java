5cc053946dc5288cafc76aabb5033c42546f22db
geeker.lait <lait.zhang@gmail.com>
2022-10-22 18:13:05 +0800

    fix

---
+++ app-api/src/main/java/org/dows/app/api/AppApplyRequest.java
@@ -0,0 +1,60 @@
+package org.dows.app.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
+public class AppApplyRequest implements Serializable {
+
+    private static final long serialVersionUID = 910895302256785608L;
+    @JsonIgnore
+    private Long id;
+
+    @ApiModelProperty("应用类型")
+    private String categId;
+
+    @ApiModelProperty("应用或小程序申请订单编号(全局唯一)")
+    private String applyOrderNo;
+
+    @ApiModelProperty("应用ID(申请审核通过后回填)")
+    private String appId;
+
+    @ApiModelProperty("应用名称")
+    private String appName;
+
+    @ApiModelProperty("")
+    private String platform;
+
+    @ApiModelProperty("第三方平台申请单号（第三放平台审核通过后回填）")
+    private String platformOrderNo;
+
+    @ApiModelProperty("第三方平台关联应用ID（第三放平台审核通过后回填）")
+    private String platformAppId;
+
+    @ApiModelProperty("第三方平台关联应用名(可以统一用appName)")
+    private String platformAppName;
+
+    @ApiModelProperty("申请人")
+    private String applicant;
+
+
+    @ApiModelProperty("")
+    private String platformAccount;
+
+    @ApiModelProperty("营业执照编号")
+    private String certNo;
+
+    @ApiModelProperty("法人")
+    private String legalName;
+
+    @ApiModelProperty("营业执照名称")
+    private String certName;
+
+    @ApiModelProperty("联系人手机")
+    private String contactPhone;
+
+    @ApiModelProperty("联系人")
+    private String contactName;
+
+}
--- app-biz/src/main/java/org/dows/app/biz/AppApplyBiz.java
+++ app-biz/src/main/java/org/dows/app/biz/AppApplyBiz.java
@@ -2,12 +2,26 @@ package org.dows.app.biz;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dows.app.api.AppApplyRequest;
+import org.dows.framework.api.Response;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class AppApplyBiz {
 
+
+    @Transactional
+    Response saveApply(AppApplyRequest appApplyRequest) {
+        // todo 保存 AppApply
+
+        // todo  保存AppApplyItem
+
+
+        return Response.ok();
+
+    }
 
 }
